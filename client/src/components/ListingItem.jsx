a97bf66df62e41279d4f575e80d1e0be0a7889f0
Suruchi <jhasuruchi237@gmail.com>
2024-05-18 22:22:13 +0530

    update contact.jsx and listitem.jsx

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -8,7 +8,7 @@ export default function ListingItem({ listing }) {
         <img
           src={
             listing.imageUrls[0] ||
-            'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
+            'https://cdn.pixabay.com/photo/2017/08/20/09/16/furniture-2660938_1280.jpg'
           }
           alt='listing cover'
           className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
