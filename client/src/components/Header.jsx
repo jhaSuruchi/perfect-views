215a3e2c026817653687b41b4fb1c40c0fff0fb6
Suruchi <jhasuruchi237@gmail.com>
2024-05-19 08:58:15 +0530

    resolve file conflicts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -65,7 +65,9 @@ export default function Header() {
                 alt='profile'
               />
             ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              <Link to='/sign-in'>
+                <li className= 'text-slate-700 hover:underline'> Sign in</li>
+              </Link>
             )}
           </Link>
         </ul>
