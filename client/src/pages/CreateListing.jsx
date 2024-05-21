a73af6974ae448e75d3716eec78a54fd82a3637f
Suruchi <jhasuruchi237@gmail.com>
2024-05-21 13:44:45 +0530

    update price display

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -273,7 +273,7 @@ export default function CreateListing() {
                 type='number'
                 id='regularPrice'
                 min='50'
-                max='10000000'
+                max='100000000'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
                 onChange={handleChange}
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -92,8 +92,8 @@ export default function Listing() {
             <p className='text-2xl font-semibold'>
               {listing.name} - ₹{' '}
               {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
+                ? (listing.regularPrice - listing.discountPrice).toLocaleString('en-IN')
+                : listing.regularPrice.toLocaleString('en-IN')}
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
@@ -106,7 +106,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ₹ {+listing.regularPrice - +listing.discountPrice} OFF
+                  ₹ {+listing.discountPrice} OFF
                 </p>
               )}
             </div>
